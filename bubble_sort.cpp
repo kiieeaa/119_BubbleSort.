a5a51bde61a006bc55b110efe981681bd9269849
kiieeaa <kiyaadinda5@gmail.com>
2026-03-03 10:52:10 +0700

    output ke layar

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -5,3 +5,11 @@ using namespace std;
 int arr[20]; // deklarasi variabel global array a dengan ukuran 20
 int n; // deklarasi variabel global n untuk menyimpan banyaknya elemen pada array
 
+void input()
+{
+    int d;
+    while (true)
+    {
+        cout << "masukan banyaknya alemen pada array : "; // output ke layar
+    }
+}
