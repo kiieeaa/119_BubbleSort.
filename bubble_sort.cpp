58205f3b64d74285e4bc6578de74f587d52d9341
kiieeaa <kiyaadinda5@gmail.com>
2026-03-03 11:37:32 +0700

    memanggil fungsi display

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -20,5 +20,86 @@ void input()
         }
 
     }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "element array yang telah tersusun" << endl;
+    cout << "====================" << endl;
+    cout << endl;
+
+    for (int j = 0; j < n; j++)
+    {                                           // looping dengan j dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (j + 1) << ": "; // output ke layar
+        cin >> arr[j]; // input dari pengguna
+    }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1; // step 1
+
+    do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        { 
+            if (arr[j] > arr[j + 1])
+            {
+                int temp ; 
+                temp =arr[j];   // step 2
+                arr[j] = arr[j + 1];  // step 3
+                arr[j + 1] = temp; 
+            }
+        }
+
+        pass += 1;  //step 4
+
+        cout << "\npass" << pass -1 << ": "; // number of pass
+        for (int k = 0; k < n; k++)
+        {
+            cout << arr[k] << " "; // menampilkan data pada number of pass
+        }
+
+        cout << endl;
+
+     /* code */
+    } while (pass < n);  // step 5
+    
+}
+
+void display()
+{
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "element array yang telah tersusun" << endl;
+    cout << "====================" << endl;
+    cout << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] ; // outpou ke lyar
+        if (j < n-1)
+        {
+            cout << ", "; // output ke layar
+        }
+
+        cout << endl;
+        cout << endl;
+
+        cout << "jumlah pass = " << n-1 <<  endl;
+        cout << endl;
+        cout << endl;
+    }
+}
+    
+    int main(){
+        input(); // memanggil fungsi input
+        bubbleSortArray(); // memanggil fungsi bubbleSortArray
+        display();
+        system("pause"); // memanggil fungsi display
+
+        return 0;
+
+    }
+
+
+
 
